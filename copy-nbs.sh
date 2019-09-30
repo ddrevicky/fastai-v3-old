7ce1a952ba0a7922975431186dd4f96766b08121
Dusan Drevicky <drevicky@gmail.com>
2019-09-30 15:43:33 +0200

    copy notebooks, update fastai aded

---
+++ copy-nbs.sh
@@ -0,0 +1,5 @@
+cp tutorials/fastai/course-v3/nbs/dl1/my* fastai-v3-notebooks/.
+cd ~/fastai-v3-notebooks
+git add -A
+git commit -m "Updated notebooks."
+git push
---
+++ update-fastai.sh
@@ -0,0 +1,4 @@
+cd tutorials/fastai/course-v3 && \
+git checkout . && \
+git pull && \
+sudo /opt/anaconda3/bin/conda install -c fastai fastai
